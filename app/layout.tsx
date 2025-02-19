40f82d19829e38cafd5452e2c458e227b3d8646a
digitallysavvy <hermes.frangoudis@gmail.com>
2025-02-18 22:48:30 -0500

    added meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   title: 'Converse with AI',
   description:
-    'A conversational AI web-app powered by OpenAI and Agora built with v0, and next.js.',
+    "A conversational AI web-app powered by Agora's Conversational AI Engine, built with next.js.",
   icons: {
     icon: [
       { url: '/favicon.ico' },
